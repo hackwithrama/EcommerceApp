445a4588eea9713a8b746237c86ce44c6991f8e8
hackwithrama <rajram92@hotmail.com>
2024-06-29 11:47:21 +0530

    Initial Commit

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Ramachandran Varadaraju on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
